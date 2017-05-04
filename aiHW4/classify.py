a82dc39aad1ab1b5437b9279bbcbee429eb2f896
Ayush Singh <ayush.vijay@gmail.com>
2017-05-04 00:35:00 -0400

    Completed algorithm selection part of code in 'train' to point to correct predictor

--- aiHW4/classify.py
+++ aiHW4/classify.py
@@ -2,7 +2,11 @@ import os
 import argparse
 import sys
 import pickle
-from Methods import ClassificationLabel, FeatureVector, Instance, Predictor, DecisionTree, NaiveBayes, NeuralNetwork
+from Methods import ClassificationLabel, FeatureVector, Instance, Predictor
+from NaiveBayes import NaiveBayes
+from DecisionTree import DecisionTree
+from NeuralNetwork import NeuralNetwork
+
 
 def load_data(filename):
 	instances = []
@@ -10,25 +14,25 @@ def load_data(filename):
 		for line in reader:
 			if len(line.strip()) == 0:
 				continue
-			
+
 			# Divide the line into features and label.
 			split_line = line.split(",")
 			label_string = split_line[0]
 
 			label = ClassificationLabel(label_string)
 			feature_vector = FeatureVector()
-			
+
 			index = 0
-			for item in split_line[1:]:  
+			for item in split_line[1:]:
 				value = float(item)
 
 				feature_vector.add(index, value)
 				index += 1
-
 			instance = Instance(feature_vector, label)
 			instances.append(instance)
 
 	return instances
+
 
 def get_args():
 	parser = argparse.ArgumentParser(description="This allows you to specify the arguments you want for classification.")
@@ -43,11 +47,13 @@ def get_args():
 
 	return args
 
+
 def predict(predictor, instances):
 	for instance in instances:
 		label = predictor.predict(instance)
 
 		print(str(label))
+
 
 def check_args(args):
 	if args.mode.lower() == "train":
@@ -57,25 +63,31 @@ def check_args(args):
 		if not os.path.exists(args.model_file):
 			raise Exception("model file specified by --model-file does not exist.")
 
+
 def train(instances, algorithm):
-	"""
-	This is where you tell classify.py what algorithm to use for training
-	The actual code for training should be in the Predictor subclasses
-	For example, if you have a subclass DecisionTree in Methods.py
-	You could say
-	if algorithm == "decision_tree":
-		predictor = DecisionTree()
-	"""
-	predictor = None # you want to change this depending on the algorithm you're using
+	# """
+	# This is where you tell classify.py what algorithm to use for training
+	# The actual code for training should be in the Predictor subclasses
+	# For example, if you have a subclass DecisionTree in Methods.py
+	# You could say
+	# if algorithm == "decision_tree":
+	# 	predictor = DecisionTree()
+	# """
+	algorithms = {
+		"decision_tree": DecisionTree(),
+		"naive_bayes": NaiveBayes(),
+		"neural_network": NeuralNetwork
+	}
+	predictor = algorithms[algorithm]
 	predictor.train(instances)
 	return predictor
+
 
 def main():
 	args = get_args()
 	if args.mode.lower() == "train":
 		# Load training data.
 		instances = load_data(args.data)
-
 		# Train
 		predictor = train(instances, args.algorithm)
 		try:
